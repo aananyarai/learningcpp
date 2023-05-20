49980fcceab6fbd28c84550aa1d7b413b2624ba2
SC2309 <121182370+sc2309@users.noreply.github.com>
2023-05-20 17:01:38 +0530

    Update Array cpp.cpp
    changed file

--- Array cpp.cpp
+++ Array cpp.cpp
@@ -6,13 +6,13 @@ using namespace std;
   {      
      cout<< "Enter any 10 numbers under 100 . "<< endl;
      
-     int myNumbers [10];
-      int i;
-     
-      for (i=1; i<10; i++) {  
-      cin>> myNumbers[i];
+     int myNumbers[10];
+      for (int i=1; i<10; i++) {  
+          cin>> myNumbers[i];
+      }
+	  for(int i=1;i<=10;i++){
+		  cout << myNumbers[i] << endl;
 	  }
      
      return 0;
-     
-	 }
+  }
